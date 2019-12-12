41f6feeb1af67ba23f723d5d62625a5f6f473257
Vitalij Sorokin <sv89@tut.by>
2019-12-12 22:27:30 +0300

    Изменён конвертер значений

--- WpfApp43S/ValueConverters/GenderConverter.cs
+++ WpfApp43S/ValueConverters/GenderConverter.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace WpfApp43S.ValueConverters
 {
     [ValueConversion(typeof(int?), typeof(int))]
-    public class GenderConverter : IValueConverter
+    public class GenderConverter : MarkupExtension, IValueConverter
     {
+        private static GenderConverter _instance;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new GenderConverter());
+        }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return value != null ? (int)value + 1 : 0;
