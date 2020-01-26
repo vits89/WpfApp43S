1739f57d1b64be2850c5734782a0a72039974e3c
Vitalij Sorokin <sv89@tut.by>
2020-01-26 15:17:56 +0300

    Добавлен конвертер значений

---
+++ WpfApp43S/ValueConverters/ValidationErrorsConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfApp43S.ValueConverters
+{
+    [ValueConversion(typeof(IEnumerable<ValidationError>), typeof(string))]
+    public class ValidationErrorsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((value is IEnumerable<ValidationError> validationErrors) && validationErrors.Any())
+            {
+                return string.Join("\n", validationErrors.Select(e => e.ErrorContent));
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
