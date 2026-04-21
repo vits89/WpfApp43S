d6448657c91ee704c5255ce4182dcb1b578bb4f4
Vitalij Sorokin <sv89@tut.by>
2026-04-21 16:12:15 +0400

    Добавлена установка ключа лицензии для AutoMapper

--- WpfApp43S/App.xaml.cs
+++ WpfApp43S/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WpfApp43S.Infrastructure;
 using WpfApp43S.Models;
@@ -28,10 +29,35 @@ public partial class App : Application
 
     private static void AddServices(IServiceCollection services)
     {
-        services.AddAutoMapper(typeof(StudentMapperProfile));
+        services.AddAutoMapper(
+            (sp, cfg) =>
+            {
+                var configuration = sp.GetRequiredService<IConfiguration>();
 
+                cfg.LicenseKey = configuration["AutoMapperLicenseKey"];
+            },
+            typeof(StudentMapperProfile));
+        services.AddLogging();
+
+        services.AddSingleton(BuildConfiguration());
         services.AddSingleton<IRepository, XmlFileRepository>();
 
         services.AddTransient<MainWindowViewModel>();
+    }
+
+    private static IConfiguration BuildConfiguration()
+    {
+        var env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+        var configurationBuilder = new ConfigurationBuilder();
+
+        configurationBuilder.AddJsonFile("appsettings.json", optional: true);
+
+        if (!string.IsNullOrWhiteSpace(env))
+        {
+            configurationBuilder.AddJsonFile($"appsettings.{env}.json", optional: true);
+        }
+
+        return configurationBuilder.Build();
     }
 }
